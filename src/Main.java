a7d05e40220e7a316aad3aadd70237b5f83d6c65
Camilo_Vargas09 <vargascardenaskevincamilo@gmail.com>
2025-08-22 17:19:14 -0500

    CAMBIOS EN EL CODIGO

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,15 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("hola mundo");
-
         System.out.println("CAMILO VARGAS");
-
         System.out.println("INGENIERO DE SISTEMAS");
+        System.out.println("hola 1");
+        System.out.println("hola 1");
+        System.out.println("hola 1");
+        System.out.println("hola 1");
+        System.out.println("hola 1");
+        System.out.println("hola 1");
+
 
 
     }
