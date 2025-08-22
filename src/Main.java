9ae50dc010a7aa3f66603657735d69b00bb103cc
Camilo_Vargas09 <vargascardenaskevincamilo@gmail.com>
2025-08-22 17:08:48 -0500

    PRIMEROS DATOS

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("hola mundo");
+
+        System.out.println("CAMILO VARGAS");
+
+        System.out.println("INGENIERO DE SISTEMAS");
+
+
+    }
+
+}
