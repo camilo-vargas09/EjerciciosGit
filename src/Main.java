8c3c0ed6e902d52cc1a8721e3ca2189c34e264e0
Camilo_Vargas09 <vargascardenaskevincamilo@gmail.com>
2025-08-28 16:52:55 -0500

    nuevos cambios

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,17 @@ public class Main {
 
         System.out.println("hola mundo");
         System.out.println("CAMILO VARGAS");
-        System.out.println("INGENIERO DE SISTEMAS");
-        System.out.println("hola 1");
-        System.out.println("hola 1");
-        System.out.println("hola 1");
-        System.out.println("hola 1");
-        System.out.println("hola 1");
-        System.out.println("hola 1");
+        System.out.println("git status - estados de las ramas");
+        System.out.println("git branch - ver ramas");
+        System.out.println("git add. - agrupar los archivos");
+        System.out.println("git commit -m - guardar cambios en local");
+        System.out.println("git push origin - subir cambios");
+        System.out.println("git log - ver todos los commit");
+        System.out.println("git config --list - listar configuraciones");
+        System.out.println("git clone - clonar proyecto");
+        System.out.println("git switch -c - crear y posicionarme sobre la rama");
+        System.out.println("git fetch - muestra las ramas remotas");
+        System.out.println("git pull origin main - bajar cambios");
 
 
 
